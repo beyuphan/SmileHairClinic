ee1af9d250fa357d2a7bd0530ff859ea1ee2e037
Eyüphan Binici <117213326+beyuphan@users.noreply.github.com>
2025-11-13 19:53:56 +0300

    admin panel randevu

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { AuthModule } from '../auth/auth.module'; // <-- GÜVENLİK İÇİN
 import { PrismaModule } from '../prisma/prisma.module'; // <-- VERİTABANI İÇİN
-import { ChatController } from './chat.controller';
+import { ChatController } from './chat.controller'; // <-- YENİ İMPORT
 
 @Module({
   imports: [AuthModule, PrismaModule], // <-- BU İKİSİNİ EKLE
-  providers: [ChatGateway], controllers: [ChatController], // Gateway'i (kapıyı) tanıt
+  providers: [ChatGateway], // Gateway'i (kapıyı) tanıt
+  controllers: [ChatController], // <-- YENİ SATIR
 })
 export class ChatModule {}
